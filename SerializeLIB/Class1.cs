2ae596f400d13c7a50a9821cfb71092d5d029ba5
nedoprogramist89 <145318384+nedoprogramist89@users.noreply.github.com>
2024-05-28 20:57:12 +0300

    Add files via upload

---
+++ SerializeLIB/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SerializeLIB
+{
+    public class Serializer
+    {
+        public async Task SerializeAsync<T>(T obj, string filePath)
+        {
+            using FileStream createStream = File.Create(filePath);
+            await JsonSerializer.SerializeAsync(createStream, obj);
+            await createStream.DisposeAsync();
+        }
+
+        public async Task<T> DeserializeAsync<T>(string filePath)
+        {
+            using FileStream openStream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(openStream);
+        }
+    }
+}
